1ec3db5658b4bbf33a3e030adcfb7cd3ad990e6e
sarahbaranski <sbaranski@gmail.com>
2021-03-14 10:43:22 -0500

    Updated Semesters show for students to request shifts with correct routes and semester index to add student name

--- src/views/SemestersShow.vue
+++ src/views/SemestersShow.vue
@@ -5,93 +5,93 @@
       <h2>Monday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="mondaymorning" value="MondayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="mondaymorning" value="Monday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="mondayafternoon" value="MondayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="mondayafternoon" value="Monday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="mondayevening" value="MondayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="mondayevening" value="Monday:evening" v-model="checkedTimes" />
       </div>
       <h2>Tuesday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="tuesdaymorning" value="TuesdayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="tuesdaymorning" value="Tuesday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="tuesdayafternoon" value="TuesdayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="tuesdayafternoon" value="Tuesday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="tuesdayevening" value="TuesdayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="tuesdayevening" value="Tuesday:evening" v-model="checkedTimes" />
       </div>
       <h2>Wednesday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="wednesdaymorning" value="WednesdayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="wednesdaymorning" value="Wednesday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="wednesdayafternoon" value="WednesdayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="wednesdayafternoon" value="Wednesday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="wednesdayevening" value="WednesdayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="wednesdayevening" value="Wednesday:evening" v-model="checkedTimes" />
       </div>
       <h2>Thursday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="thursdaymorning" value="ThursdayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="thursdaymorning" value="Thursday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="thursdayafternoon" value="ThursdayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="thursdayafternoon" value="Thursday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="thursdayevening" value="ThursdayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="thursdayevening" value="Thursday:evening" v-model="checkedTimes" />
       </div>
       <h2>Friday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="fridaymorning" value="FridayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="fridaymorning" value="Friday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="fridayafternoon" value="FridayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="fridayafternoon" value="Friday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="fridayevening" value="FridayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="fridayevening" value="Friday:evening" v-model="checkedTimes" />
       </div>
       <h2>Saturday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="saturdaymorning" value="SaturdayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="saturdaymorning" value="Saturday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="saturdayafternoon" value="SaturdayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="saturdayafternoon" value="Saturday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="saturdayevening" value="SaturdayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="saturdayevening" value="Saturday:evening" v-model="checkedTimes" />
       </div>
       <h2>Sunday:</h2>
       <div>
         Morning:
-        <input type="checkbox" id="sundaymorning" value="SundayMorning" v-model="checkedTimes" />
+        <input type="checkbox" id="sundaymorning" value="Sunday:morning" v-model="checkedTimes" />
       </div>
       <div>
         Afternoon:
-        <input type="checkbox" id="sundayafternoon" value="SundayAfternoon" v-model="checkedTimes" />
+        <input type="checkbox" id="sundayafternoon" value="Sunday:afternoon" v-model="checkedTimes" />
       </div>
       <div>
         Evening:
-        <input type="checkbox" id="sundayevening" value="SundayEvening" v-model="checkedTimes" />
+        <input type="checkbox" id="sundayevening" value="Sunday:evening" v-model="checkedTimes" />
       </div>
       <input type="submit" value="Create" />
     </form>
@@ -109,18 +109,16 @@ export default {
   created: function() {},
   methods: {
     createShift: function() {
+      console.log(this.checkedTimes);
       var params = {
-        // semester: this.semester.id,
-        day: this.newday,
-        MorningTime: this.newMorningTime,
-        AfternoonTime: this.newAfternoonTime,
-        EveningTime: this.newEveningTime,
+        semester_id: this.$route.params.id,
+        checked_times: this.checkedTimes,
       };
       axios
-        .post("/api/shifts", params)
+        .post("/api/shifts_all", params)
         .then(response => {
           console.log("shifts create", response);
-          this.$router.push("/shifts");
+          // this.$router.push("/shifts");
         })
         .catch(error => {
           console.log("shifts create error", error.response);
--- src/views/ShiftsIndex.vue
+++ src/views/ShiftsIndex.vue
@@ -3,7 +3,7 @@
     <h1>All Students Availability</h1>
     <div v-for="shift in shifts" v-bind:key="shift.id">
       <div v-if="shift.requested === true">
-        <h2>{{ shift.student_id }}</h2>
+        <h2>{{ shift.student_name }}</h2>
         <p>Day: {{ shift.day }}</p>
         <p>Time: {{ shift.time }}</p>
       </div>
