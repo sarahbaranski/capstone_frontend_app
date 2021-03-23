a1d863664a57ee01b9e040eb0cdd1b0a6d93df1b
sarahbaranski <sbaranski@gmail.com>
2021-03-23 11:52:25 -0500

    changed width, margin top and bottom and padding for input forms

--- src/views/SemestersNew.vue
+++ src/views/SemestersNew.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="semesters-new">
-    <h1>New Semester</h1>
+    <h1>Create a New Semester</h1>
     <form v-on:submit.prevent="createSemester()">
       <ul>
         <li v-for="error in errors" v-bind:key="error.id">{{ error }}</li>
@@ -11,10 +11,31 @@
       <input type="date" v-model="newSemesterStartDate" />
       End Date:
       <input type="date" v-model="newSemesterEndDate" />
+      <br />
       <input type="submit" value="Create" />
     </form>
   </div>
 </template>
+
+<style scoped>
+.semesters-new {
+  margin: 4em;
+}
+form {
+  padding: 2em;
+}
+input[type="date"] {
+  width: 65%;
+  margin-top: 0.25em;
+  margin-bottom: 0.75em;
+}
+
+input[type="text"] {
+  width: 65%;
+  margin-top: 0.25em;
+  margin-bottom: 0.75em;
+}
+</style>
 
 <script>
 import axios from "axios";
